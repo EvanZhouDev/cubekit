c1bcdedce9fd407e44accc1a5e377505d9b83a8d
EvanZhouDev <evanzhouyifan@gmail.com>
2022-12-16 15:14:16 -0800

    ✨ Added some more logic

--- cube3.js
+++ cube3.js
@@ -1,3 +1,5 @@
+// ! need to add cube rotations!
+
 // Simple function to perform matrix multiplication
 let mMult = (A, B) =>
 	A.map((row, i) =>
@@ -38,7 +40,9 @@ class Piece {
 	}
 
 	tx = (a) => {
-		[this.c[1], this.c[2]] = [this.c[2], this.c[1]]; // switch correct colors
+		for (let i = 0; i < Math.abs((a / Math.PI) * 2) % 2; i++) {
+			[this.c[1], this.c[2]] = [this.c[2], this.c[1]]; // switch correct colors
+		}
 		this.p = mTrans(mMult(rx(a), mTrans([this.p])))[0].map((x) =>
 			Math.round(x)
 		); // multiply position by rotation matrix
@@ -47,49 +51,200 @@ class Piece {
 	};
 
 	ty = (a) => {
-		[this.c[0], this.c[2]] = [this.c[2], this.c[0]];
+		for (let i = 0; (i < Math.abs((a / Math.PI) * 2)) % 2; i++) {
+			[this.c[0], this.c[2]] = [this.c[2], this.c[0]];
+		}
 		this.p = mTrans(mMult(ry(a), mTrans([this.p])))[0].map((x) =>
 			Math.round(x)
 		);
 	};
 
 	tz = (a) => {
-		[this.c[0], this.c[1]] = [this.c[1], this.c[0]];
+		for (let i = 0; i < Math.abs((a / Math.PI) * 2) % 2; i++) {
+			[this.c[0], this.c[1]] = [this.c[1], this.c[0]];
+		}
 		this.p = mTrans(mMult(rz(a), mTrans([this.p])))[0].map((x) =>
 			Math.round(x)
 		);
 	};
 }
 
-let pieces = []; // unordered list of all the pieces
+const WideMoves = {
+	Fw: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z >= 0,
+		axis: "z",
+	},
+	Bw: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z <= 0,
+		axis: "z",
+	},
+	Uw: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y >= 0,
+		zCond: (z) => z !== undefined,
+		axis: "y",
+	},
+	Dw: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y <= 0,
+		zCond: (z) => z !== undefined,
+		axis: "y",
+	},
+	Lw: {
+		xCond: (x) => x <= 0,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z !== undefined,
+		axis: "x",
+	},
+	Rw: {
+		xCond: (x) => x >= 0,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z !== undefined,
+		axis: "x",
+	},
+};
 
-// Color Scheme
-let cs = {
+const WideMovesUNSAFE = {
+	f: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z >= 0,
+		axis: "z",
+	},
+	b: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z <= 0,
+		axis: "z",
+	},
+	u: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y >= 0,
+		zCond: (z) => z !== undefined,
+		axis: "y",
+	},
+	d: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y <= 0,
+		zCond: (z) => z !== undefined,
+		axis: "y",
+	},
+	l: {
+		xCond: (x) => x <= 0,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z !== undefined,
+		axis: "x",
+	},
+	r: {
+		xCond: (x) => x >= 0,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z !== undefined,
+		axis: "x",
+	},
+};
+
+const StandardMoves = {
+	F: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z === 1,
+		axis: "z",
+	},
+	B: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z === -1,
+		axis: "z",
+	},
+	U: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y === 1,
+		zCond: (z) => z !== undefined,
+		axis: "y",
+	},
+	D: {
+		xCond: (x) => x !== undefined,
+		yCond: (y) => y === -1,
+		zCond: (z) => z !== undefined,
+		axis: "y",
+	},
+	L: {
+		xCond: (x) => x === -1,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z !== undefined,
+		axis: "x",
+	},
+	R: {
+		xCond: (x) => x === 1,
+		yCond: (y) => y !== undefined,
+		zCond: (z) => z !== undefined,
+		axis: "x",
+	},
+};
+
+export const WesternColors = {
 	x: ["O", "R"], // Negative x, positive x
 	y: ["Y", "W"], // Negative y, positive y
 	z: ["B", "G"], // Negative z, positive z
 };
 
+export const JapaneseColors = {
+	x: ["O", "R"], // Negative x, positive x
+	y: ["B", "W"], // Negative y, positive y
+	z: ["Y", "G"], // Negative z, positive z
+};
+
 class Cube {
-	constructor() {
+	#initialized = false;
+	constructor(...moveSet) {
 		this.pieces = [];
-		this.init();
+		if (moveSet.length == 0) {
+			console.info(
+				"No moves have been added to your current cube. Add moves after setup by using 'Cube.registerMoveset()'"
+			);
+		}
+		this.registerMoveset(...moveSet);
+		// Color Scheme
+		this.cs;
 	}
 
-	init = () => {
-		// initialize all of the necessary things to simulate a cube
-		for (let x = -1; x <= 1; x++) {
-			// loop through all the possible locations on a 3x3
-			for (let y = -1; y <= 1; y++) {
-				for (let z = -1; z <= 1; z++) {
-					let cx = x == -1 ? cs.x[0] : x == 1 ? cs.x[1] : undefined,
-						cy = y == -1 ? cs.y[0] : y == 1 ? cs.y[1] : undefined,
-						cz = z == -1 ? cs.z[0] : z == 1 ? cs.z[1] : undefined;
-					if (!(x == 0 && y == 0 && z == 0)) {
-						this.pieces.push(new Piece([x, y, z], [cx, cy, cz]));
+	registerMoveset(...moveSet) {
+		for (let j = 0; j < moveSet.length; j++) {
+			let moves = moveSet[j];
+			for (let i = 0; i < Object.keys(moves).length; i++) {
+				let mn = Object.keys(moves)[i];
+				let mi = moves[mn];
+				this[mn] = (a) => {
+					this.turn(mi.xCond, mi.yCond, mi.zCond, mi.axis, (-Math.PI / 2) * a);
+				};
+			}
+		}
+	}
+
+	init = (cs = WesternColors) => {
+		if (!this.#initialized) {
+			this.cs = cs;
+			// initialize all of the necessary things to simulate a cube
+			for (let x = -1; x <= 1; x++) {
+				// loop through all the possible locations on a 3x3
+				for (let y = -1; y <= 1; y++) {
+					for (let z = -1; z <= 1; z++) {
+						let cx = x == -1 ? cs.x[0] : x == 1 ? cs.x[1] : undefined,
+							cy = y == -1 ? cs.y[0] : y == 1 ? cs.y[1] : undefined,
+							cz = z == -1 ? cs.z[0] : z == 1 ? cs.z[1] : undefined;
+						if (!(x == 0 && y == 0 && z == 0)) {
+							this.pieces.push(new Piece([x, y, z], [cx, cy, cz]));
+						}
 					}
 				}
 			}
+			this.#initialized = true;
+		} else {
+			console.error("Cannot initialize the same Rubik's Cube twice.");
 		}
 	};
 
@@ -159,132 +314,115 @@ class Cube {
 		}
 	};
 
-	// positive 'a' is clockwise multiplicity of a quarter turn
-	// negative 'a' is counterclockwise multiplicity of a quarter turn
-	F = (a) => {
-		this.turn(
-			(x) => x !== undefined,
-			(y) => y !== undefined,
-			(z) => z === 1,
-			"z",
-			(-Math.PI / 2) * a
-		);
+	isSolved = () => {
+		let flattened = this.flatten();
+		for (let i = 0; i < Object.keys(flattened).length; i++) {
+			let face = Object.keys(flattened)[i];
+			for (let y = 0; y < flattened[face].length; y++) {
+				for (let x = 0; x < flattened[face][y].length; x++) {
+					if (flattened[face][y][x] !== flattened[face][0][0]) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
 	};
+}
 
-	B = (a) => {
-		this.turn(
-			(x) => x !== undefined,
-			(y) => y !== undefined,
-			(z) => z === -1,
-			"z",
-			(-Math.PI / 2) * a
-		);
-	};
+class Parser {
+	constructor(cube, ...algorithms) {
+		this.cube = cube;
+		this.algorithms = algorithms;
+	}
 
-	U = (a) => {
-		this.turn(
-			(x) => x !== undefined,
-			(y) => y !== 1,
-			(z) => z !== undefined,
-			"y",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	D = (a) => {
-		this.turn(
-			(x) => x !== undefined,
-			(y) => y !== -1,
-			(z) => z !== undefined,
-			"y",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	L = (a) => {
-		this.turn(
-			(x) => x !== 1,
-			(y) => y !== undefined,
-			(z) => z !== undefined,
+	exec(algorithm) {
+		let algArr = algorithm.split(" ");
+		let allowedMoves = [
+			"R",
+			"L",
+			"U",
+			"D",
+			"B",
+			"F",
+			"r",
+			"l",
+			"u",
+			"d",
+			"b",
+			"f",
 			"x",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	R = (a) => {
-		this.turn(
-			(x) => x !== -1,
-			(y) => y !== undefined,
-			(z) => z !== undefined,
-			"x",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	Fw = (a) => {
-		this.turn(
-			(x) => x !== undefined,
-			(y) => y !== undefined,
-			(z) => z >= 0,
-			"z",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	Bw = (a) => {
-		this.turn(
-			(x) => x !== undefined,
-			(y) => y !== undefined,
-			(z) => z <= 0,
-			"z",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	Uw = (a) => {
-		this.turn(
-			(x) => x !== undefined,
-			(y) => y >= 0,
-			(z) => z !== undefined,
 			"y",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	Dw = (a) => {
-		this.turn(
-			(x) => x !== undefined,
-			(y) => y <= 0,
-			(z) => z !== undefined,
-			"y",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	Lw = (a) => {
-		this.turn(
-			(x) => x <= 0,
-			(y) => y !== undefined,
-			(z) => z !== undefined,
-			"x",
-			(-Math.PI / 2) * a
-		);
-	};
-
-	Rw = (a) => {
-		this.turn(
-			(x) => x >= 0,
-			(y) => y !== undefined,
-			(z) => z !== undefined,
-			"x",
-			(-Math.PI / 2) * a
-		);
-	};
+			"z",
+		];
+		for (let i = 0; i < algArr.length; i++) {
+			let currentNotation = algArr[i];
+			let multiplier = currentNotation.includes("'") ? -1 : 1;
+			currentNotation = currentNotation.replace("'", "");
+			let wWide = currentNotation[1] == "w";
+			let lowerWide = false;
+			currentNotation = currentNotation.replace("w", "");
+			if (allowedMoves.includes(currentNotation[0])) {
+				// normal move
+				if (currentNotation[0] === currentNotation[0].toLowerCase()) {
+					lowerWide = true;
+					currentNotation =
+						currentNotation[0].toUpperCase() +
+						currentNotation.substr(1, currentNotation.length - 1);
+				}
+				let moveType = currentNotation[0];
+				if (lowerWide) {
+					currentNotation = currentNotation.slice(lowerWide);
+				} else {
+					currentNotation = currentNotation.slice(wWide + 1);
+				}
+				let parsedNum = Number.parseInt(currentNotation);
+				let turnMult = isNaN(parsedNum) ? 1 : parsedNum;
+				console.log(
+					moveType + (wWide || lowerWide ? "w" : ""),
+					multiplier * turnMult
+				);
+				this.cube[moveType + (wWide || lowerWide ? "w" : "")](
+					multiplier * turnMult
+				);
+			} else {
+				console.error("Cannot parse move " + currentNotation);
+			}
+		}
+	}
 }
 
 // Apply a transformation to pieces that fall under the given conditions
 
-let c = new Cube();
+// Extracted cube as a reusable object
+
+let c = new Cube(StandardMoves);
+c.init();
+let parser = new Parser(c);
+parser.exec("L2 R2 U2 D2 F2 B2");
 console.log(c.flatten());
-c.Fw(-1);
-console.log(c.flatten());
+
+// console.log(c);
+// c.Rw(2);
+// c.Rw(1);
+// console.log(c.pieces);
+/*
+R -1 wide
+U 2 
+R -2 
+U -1 
+*/
+
+// // Registers a moveset to be able to be used for a modular system
+// c.registerMoveset(WideMovesUNSAFE); // lowercase letters for wide moves
+// c.registerMoveset(WideMoves); // move + "w" for wide moves
+
+// Now we can use our moves!
+// console.log(c.f(-1));
+// console.log(c.Fw(-1));
+
+export { Cube, StandardMoves, WideMoves, WideMovesUNSAFE };
+
+// console.log(c.isSolved());
+// console.log(c.flatten());
+// console.log(c.isSolved());
